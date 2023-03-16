21c27c8e69b2dbd9db937cabb2b0e5c5436f299e
swiftreplyai <127563901+swiftreplyai@users.noreply.github.com>
2023-03-16 12:19:43 -0400

    Update titan.py

--- titan.py
+++ titan.py
@@ -4,36 +4,37 @@ import openai
 secrets = st.secrets["openai"]
 openai.api_key = secrets["api_key"]
 
-# Step 3: Initialize Streamlit app
 st.title("Itinerary Generator with ChatGPT")
+st.markdown("Enter the destination, number of days, and your interests, then click 'Generate Itinerary'.")
 
-# Step 4: Set up app layout and widgets
-destination = st.text_input("Enter the destination:")
-days = st.number_input("Enter the number of days:", min_value=1, value=1)
-interests = st.multiselect("Select your interests:", options=['Culture', 'Adventure', 'Food', 'Nature', 'Shopping', 'Nightlife', 'Relaxation'])
+model_engine = "text-davinci-002"
+openai.api_key = secrets["api_key"]
 
-generate_button = st.button("Generate Itinerary")
-
-# Step 5: Create a function to generate itinerary using ChatGPT
 def generate_itinerary(destination, days, interests):
-    openai.api_key = secrets["api_key"]
     prompt = f"Create a detailed itinerary for a {days}-day trip to {destination} with a focus on the following interests: {', '.join(interests)}. Include recommended activities, accommodations, and other suggestions."
+
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine=model_engine,
         prompt=prompt,
         max_tokens=500,
         n=1,
         stop=None,
         temperature=0.7,
     )
+
     return response.choices[0].text.strip()
 
-# Step 6: Call the function with the user's input destination, days, and interests
-if generate_button:
-    if destination and days and interests:
-        generated_itinerary = generate_itinerary(destination, days, interests)
+def main():
+    destination = st.text_input("Enter the destination:")
+    days = st.number_input("Enter the number of days:", min_value=1, value=1)
+    interests = st.multiselect("Select your interests:", options=['Culture', 'Adventure', 'Food', 'Nature', 'Shopping', 'Nightlife', 'Relaxation'])
 
-        # Step 7: Display the generated itinerary in the Streamlit app
-        st.write(generated_itinerary)
-    else:
-        st.write("Please provide all the required information.")
+    if st.button("Generate Itinerary"):
+        if destination and days and interests:
+            generated_itinerary = generate_itinerary(destination, days, interests)
+            st.write(generated_itinerary)
+        else:
+            st.write("Please provide all the required information.")
+
+if __name__ == '__main__':
+    main()
